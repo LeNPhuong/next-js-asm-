18ba4ed38fa8a4e2952499715a82f5bcfe1b23d1
Le Nguyen Phuong <phuongproga73@gmail.com>
2024-08-02 10:38:43 +0700

    up

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,6 @@ const nextConfig = {
     domains: ['cdn.tgdd.vn'],
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
 }
